ca32231123f67e0d4e1c031cd2b22141372b7a7d
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2020-10-19 23:42:01 -0500

    Owner list y edit/add ready

--- src/app/owner-edit/owner-edit.component.ts
+++ src/app/owner-edit/owner-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OwnerService } from '../shared/owner/owner.service';
+import { GiphyService } from '../shared/giphy/giphy.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-owner-edit',
@@ -6,10 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-edit.component.css']
 })
 export class OwnerEditComponent implements OnInit {
+  owner: any = {};
+  API = 'http://thawing-chamber-47973.herokuapp.com';
+  OWNER_API = this.API + '/owners/';
 
-  constructor() { }
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private ownerService: OwnerService,
+              private giphyService: GiphyService) {
+  }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      var urlOwner = params['id'];  
+      
+      if (urlOwner) {
+        const id = urlOwner.split(this.OWNER_API,this.OWNER_API.length)[1];
+        this.ownerService.get(id).subscribe((owner: any) => {
+          if (owner) {
+            this.owner = owner;
+            this.owner.href = owner._links.self.href;
+            this.giphyService.get(owner.name).subscribe(url => owner.giphyUrl = url);
+          } else {
+            console.log(`Owner with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/owner-list']);
+  }
+
+  save(form: NgForm) {
+    this.ownerService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.ownerService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
   }
 
 }
